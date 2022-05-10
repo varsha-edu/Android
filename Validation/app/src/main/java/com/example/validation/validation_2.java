6447e6c082fcfd19e7054acffc0352902d857155
varsha-edu <varsha.p.2019.cse@rajalakshmi.edu.in>
2022-05-10 09:03:17 +0530

    Validation

---
+++ Validation/app/src/main/java/com/example/validation/validation_2.java
@@ -0,0 +1,14 @@
+package com.example.validation;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class validation_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_validation2);
+    }
+}
